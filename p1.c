09793a5a1ec40fe8e6aa6097ba2469c966f0f7a2
alifoo <alissonayres.trb@gmail.com>
2025-11-16 21:16:06 -0300

    fix: typo

--- p1.c
+++ p1.c
@@ -1,11 +1,10 @@
 #include "arquivo.h"
 #include <stdio.h>
 
-
 int main() {
     printf("Convertendo CSV para binário...\n\n");
     csv_to_bin("alimentos.csv", "dados.bin");
     printf("\nConversão concluída.\n");
-    
+
     return 0;
 }
